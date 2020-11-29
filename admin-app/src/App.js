9027dab71dfbd3e875e1bb73f467f5a72c600e10
imran <imran.mia@portonics.com>
2020-11-30 01:34:14 +0600

    admin panel

---
+++ admin-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Header from '../src/Component/Layout/Header'
+function App() {
+  return (
+    <div className="App">
+       <Header></Header>
+    </div>
+  );
+}
+
+export default App;
