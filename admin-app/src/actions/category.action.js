3e07f1d2caca06bf8cb83cb407b25a862b7b533b
imran <imran.mia@portonics.com>
2021-01-16 13:41:05 +0600

    jwt bug fix

--- admin-app/src/actions/category.action.js
+++ admin-app/src/actions/category.action.js
@@ -4,42 +4,47 @@ import { categoryConstants } from "./constance"
 export const getAllCategory = () => {
     return async dispatch => {
 
-        dispatch({type: categoryConstants.GET_ALL_CATEGORY_REQUEST})
+        dispatch({ type: categoryConstants.GET_ALL_CATEGORY_REQUEST })
 
         const res = await axios.get('/category/getcategory');
         console.log(res)
 
-        if(res.status === 200){
+        if (res.status === 200) {
             const { categoryList } = res.data;
             dispatch({
-                 type: categoryConstants.GET_ALL_CATEGORY_SUCCESS,
-                 payload: {categories: categoryList}
+                type: categoryConstants.GET_ALL_CATEGORY_SUCCESS,
+                payload: { categories: categoryList }
             })
-        }else{
+        } else {
             dispatch({
                 type: categoryConstants.GET_ALL_CATEGORY_FAILURE,
                 payload: { error: res.data.error }
-           })
+            })
         }
     }
 }
 
 export const addCategory = (form) => {
     return async dispatch => {
-        dispatch({type: categoryConstants.ADD_NEW_CATEGORY_REQUEST})
-        const res = await axios.post('/category/create', form);
-        console.log(res)
-         if (res.status === 201){
-            dispatch({
-                type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
-                payload: {category: res.data.category}
-            })
-        }else {
-            dispatch({
-                type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
-                payload: res.data.error
-            })
+        dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST });
+        try {
+            const res = await axios.post('/category/create', form);
+            console.log(res)
+            if (res.status === 201) {
+                dispatch({
+                    type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+                    payload: { category: res.data.category }
+                })
+            } else {
+                dispatch({
+                    type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+                    payload: res.data.error
+                })
+            }
+        } catch (error) {
+            console.log(error.res)
         }
+
     }
 }
 
@@ -47,10 +52,10 @@ export const addCategory = (form) => {
 export const updateCategorys = (form) => {
     return async dispatch => {
         const res = await axios.post('/category/update', form);
-        console.log("res-update",res)
-         if (res.status === 201){
-             return true
-        }else {
+        console.log("res-update", res)
+        if (res.status === 201) {
+            return true
+        } else {
             return false
         }
     }
@@ -63,11 +68,11 @@ export const deleteCategorys = (ids) => {
                 ids: ids
             }
         });
-        if (res.status === 200){
+        if (res.status === 200) {
             return true
-        }else {
+        } else {
             return false
         }
-    
+
     }
 }
--- admin-app/src/helper/axios.js
+++ admin-app/src/helper/axios.js
@@ -1,12 +1,36 @@
 import axios from 'axios';
 import { api } from '../UrlConfig';
+import store from '../store';
+import { authConstants } from '../actions/constance';
 
-const token = window.localStorage.getItem('token');
+const token =  window.localStorage.getItem('token');
 
 const axiosInstance = axios.create({
     baseURL: api,
     headers: {
         'Authorization': token ? `Bearer ${token}` : '' 
+    }
+});
+
+ 
+axiosInstance.interceptors.request.use((req) => { // Req new token
+    const { auth } = store.getState();
+    if(auth.token){
+        req.headers.Authorization = `Bearer ${auth.token}`
+    }
+
+    return req;
+ });
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use((res) => {
+   return res
+}, (error)=> {
+    console.log("interceptor", error.response);
+    const { status } = error.response;
+    if (status === 500 || status === 400){
+        localStorage.clear()
+        store.dispatch( { type: authConstants.LOGUOUT_SUCCESS })
     }
 });
 
